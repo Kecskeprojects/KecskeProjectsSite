9da104a55d9684d50dc149a603ab9bd4ed8a6e18
Kecskeprojects <kirsch.a.p@gmail.com>
2025-11-20 20:32:42 +0100

    Delete GlobalSuppressions.cs

--- Backend/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Performance", "CA1873:Avoid potentially expensive logging", Justification = "<Pending>", Scope = "member", Target = "~M:Backend.Services.FileStorageService.SaveViaMultipartReaderAsync(System.String,System.Boolean,System.String,System.IO.Stream,System.Threading.CancellationToken)~System.Threading.Tasks.Task{System.String}")]
