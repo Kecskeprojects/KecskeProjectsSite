c6d062d75adb80ae1a455a560efe238b80dabfd2
Kecskeprojects <kirsch.a.p@gmail.com>
2025-11-24 02:08:10 +0100

    removed unused classes

--- Backend/Communication/Outgoing/ErrorActionResult.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Backend.Communication.Outgoing;
-
-public class ErrorActionResult(string errorMessage)
-{
-    public string Error { get; private set; } = errorMessage;
-}
--- Backend/Communication/Outgoing/MessageActionResult.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Backend.Communication.Outgoing;
-
-public class MessageActionResult(string message)
-{
-    public string Message { get; private set; } = message;
-}
